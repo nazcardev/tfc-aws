f0481d776bf46c7e463ae4480641231e457a66c3
nazcardev <165267480+nazcardev@users.noreply.github.com>
2024-12-10 23:52:16 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
     organization = "ACME_Corporation" 
 
     workspaces { 
-      name = "AWS" 
+      name = "AWS-VCS" 
     } 
   } 
   required_providers {
@@ -24,6 +24,7 @@ provider "aws" {
 resource "aws_instance" "frontend_app_server" {
   ami           = "ami-091b8df234926413f"
   instance_type = "t2.micro"
+  key_name = "aws-key"
 
   tags = {
     Name = "frontend"
@@ -33,13 +34,13 @@ resource "aws_instance" "frontend_app_server" {
 resource "aws_instance" "backend_app_server" {
   ami           = "ami-091b8df234926413f"
   instance_type = "t2.micro"
-
+  key_name = "aws-key"
   tags = {
     Name = "backend"
   }
 }
 
-resource "aws_key_pair" "ssh-key" {
+resource "aws_key_pair" "aws-key" {
   key_name   = "aws-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7LhhH1sqVLQ7TNBJSHCEEo+YnOLhETxXPcIkxy1SDzQ3pBr4Z5lHCalld0xnwQrx0TXV2A6RopieUZMo0ki/LB/d76Hq3q4+wKn/zkmu5qqUsl0vpaNSz8+oUI+7Wm6onR731psa7IYWE9uucL9LH2TmRz5okDOFDXJNoGRO+1Yk20EllTJn9BiWu4ixYPYDN7LSvsYyyFPsoAJZ3ZMajiLEN95h7zvZz5uv1zQFAHOT1LVrpcyTmRq4Nr1LPzLaFVmRKX5KISqvylBVUecWUj1N/yW1XzimoQh18iQKb9Xh+d7PbLAiFig6PeYIBmcm7iIpa5CEqSxYsTO09MLzCREzlxuGAzIiDLmGgKIuBO55FelxCju/a8lvb9EhzMrPNcAPXBpuUenV81wz14+uxaai8Pf4k8+wLPlSRXfWubFiDBGkVHXKuvbaYvvVz9NR5/8XzDFf/JkQ6Ffztr+tLZJDzyVhqU1nBYrH/qU1WFWxUC9hHpaklyWNQmWv2PFE= nmiah@nmiah1-mac"
 }
