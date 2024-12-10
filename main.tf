b2e125736c438d0cbe4e4b19e43d3acdc4cf5e0d
nazcardev <165267480+nazcardev@users.noreply.github.com>
2024-12-10 22:59:25 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  cloud { 
+    
+    organization = "ACME_Corporation" 
+
+    workspaces { 
+      name = "AWS" 
+    } 
+  } 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "frontend_app_server" {
+  ami           = "ami-091b8df234926413f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "backend_app_server" {
+  ami           = "ami-091b8df234926413f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "backend"
+  }
+}
